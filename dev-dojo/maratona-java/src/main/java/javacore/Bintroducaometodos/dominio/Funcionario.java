508dc5a7decc3cd9e0cf281eb70cc2923fdc433d
Waidiney <dineyjr@hotmail.com>
2022-07-08 10:19:54 -0300

    Automatização Get/Set

--- dev-dojo/maratona-java/src/main/java/javacore/Bintroducaometodos/dominio/Funcionario.java
+++ dev-dojo/maratona-java/src/main/java/javacore/Bintroducaometodos/dominio/Funcionario.java
@@ -1,10 +1,13 @@
 package javacore.Bintroducaometodos.dominio;
 
 public class Funcionario {
-    public String nome;
-    public int idade;
-    public double[] salarios;
-
+    private String nome;
+    private int idade;
+    private double[] salarios;
+    private double media = 0;
+    public double getMedia() {
+        return media;
+    }
     public void imprime() {
         System.out.println(this.nome);
         System.out.println(this.idade);
@@ -13,6 +16,31 @@ public class Funcionario {
         }
         imprimeMediaSalario();
     }
+    // Alt + insert mostra opção de inserir automáticamente os get's e set's em cada classe privada
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public double[] getSalarios() {
+        return salarios;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setSalarios(double[] salarios) {
+        this.salarios = salarios;
+    }
+
     public void imprimeMediaSalario() {
         double media = 0;
         for(double salario: salarios){
--- dev-dojo/maratona-java/src/main/java/javacore/Bintroducaometodos/test/FuncionarioTeste01.java
+++ dev-dojo/maratona-java/src/main/java/javacore/Bintroducaometodos/test/FuncionarioTeste01.java
@@ -5,10 +5,10 @@ import javacore.Bintroducaometodos.dominio.Funcionario;
 public class FuncionarioTeste01 {
     public static void main(String[] args) {
         Funcionario funcionario = new Funcionario();
-        funcionario.nome = "Waidy";
-        funcionario.idade = 34;
-        funcionario.salarios = new double[]{1200, 987.32, 2000};
-
+        funcionario.setNome ("Waidy");
+        funcionario.setIdade(34);
+        funcionario.setSalarios(new double[]{1200, 987.32, 2000});
         funcionario.imprime();
+        System.out.println("Média " + funcionario.getMedia() );
     }
 }
