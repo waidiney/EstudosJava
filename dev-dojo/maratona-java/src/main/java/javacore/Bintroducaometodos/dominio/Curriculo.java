37ae79a9973d2b0612cd9a3c5ab3c7f262d30487
Waidiney <dineyjr@hotmail.com>
2022-07-12 11:39:27 -0300

    Configuração Set
    Configurando os Set's das variáveis para aprimoramento do código

--- dev-dojo/maratona-java/src/main/java/javacore/Bintroducaometodos/dominio/Curriculo.java
+++ dev-dojo/maratona-java/src/main/java/javacore/Bintroducaometodos/dominio/Curriculo.java
@@ -1,17 +1,61 @@
 package javacore.Bintroducaometodos.dominio;
 
 public class Curriculo {
-     public String nome;
-     public int idade;
-     public double sexo;
-     public String estadocivil;
-     public String experiencia;
-     public String apto;
-     public String empregado;
-     public double cargo;
-     public String pretensão;
+    private String nome;
+    private int idade;
+    private String sexo;
+    private String estadocivil;
+    private String experiencia;
+    private boolean apto;
+    private boolean empregado;
+    private String cargo;
+    private int pretensao;
 
+    public void imprime() {
+        System.out.println(this.nome);
+        System.out.println(this.idade);
+        System.out.println(this.sexo);
+        System.out.println(this.estadocivil);
+        System.out.println(this.experiencia);
+        System.out.println(this.apto);
+        System.out.println(this.empregado);
+        System.out.println(this.cargo);
+        System.out.println(this.pretensao);
+    }
 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
 
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public void setEstadocivil(String estadocivil) {
+        this.estadocivil = estadocivil;
+    }
+
+    public void setExperiencia(String experiencia) {
+        this.experiencia = experiencia;
+    }
+
+    public void setApto(boolean apto) {
+        this.apto = apto;
+    }
+
+    public void setEmpregado(boolean empregado) {
+        this.empregado = empregado;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public void setPretensao(int pretensao) {
+        this.pretensao = pretensao;
+    }
 }
--- dev-dojo/maratona-java/src/main/java/javacore/Bintroducaometodos/test/CurriculoTest01.java
+++ dev-dojo/maratona-java/src/main/java/javacore/Bintroducaometodos/test/CurriculoTest01.java
@@ -1,7 +1,19 @@
 package javacore.Bintroducaometodos.test;
 
+import javacore.Bintroducaometodos.dominio.Curriculo;
+
 public class CurriculoTest01 {
     public static void main(String[] args) {
-        CurriculoTest01 curriculoTest01;
+        Curriculo curriculo = new Curriculo();
+        curriculo.setNome("Waidiney");
+        curriculo.setIdade(34);
+        curriculo.setSexo("Masculino");
+        curriculo.setEstadocivil("Casado");
+        curriculo.setExperiencia("6 meses de experiência");
+        curriculo.setApto(true);
+        curriculo.setEmpregado(false);
+        curriculo.setCargo("Estagiário");
+        curriculo.setPretensao(5000);
+        curriculo.imprime();
     }
 }
