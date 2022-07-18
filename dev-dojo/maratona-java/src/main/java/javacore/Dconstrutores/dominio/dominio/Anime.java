e4c945b1d1455d96bd37a89922d242997baa4c3e
Waidiney <dineyjr@hotmail.com>
2022-07-18 11:06:38 -0300

    Construtores parte 1

---
+++ dev-dojo/maratona-java/src/main/java/javacore/Dconstrutores/dominio/dominio/Anime.java
@@ -0,0 +1,74 @@
+package javacore.Dconstrutores.dominio.dominio;
+
+public class Anime {
+    private String nome;
+    private String tipo;
+    private int episodios;
+    private String genero;
+
+    //------------------------------------------------------------------------------------------
+    //Dentro desta área está representado um construtor
+    public Anime(String nome, String tipo, int episodios, String genero) {
+        System.out.println("Dentro do Construtor");
+        this.nome = nome;
+        this.tipo = tipo;
+        this.episodios = episodios;
+        this.genero = genero;
+    }
+    //-------------------------------------------------------------------------------------------
+    // Trecho abaixo removido, pois agora estamos chamando e configurando o objeto direto no construtor
+    //  public void init(String nome, String tipo, int episodios) {
+    //  this.nome = nome;
+    //  this.tipo = tipo;
+    //  this.episodios = episodios;
+    //  }
+
+    //  public void init(String nome, String tipo, int episodios, String genero) {
+    //  this.init(nome, tipo, episodios);
+    //  this.genero = genero;
+    //  }
+
+    public void imprime() {
+        System.out.println(this.nome);
+        System.out.println(this.tipo);
+        System.out.println(this.episodios);
+        System.out.println(this.genero);
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public String getGenero(String ação) {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getTipo() {
+        return this.tipo;
+    }
+
+    public void setEpisodios(int episodios) {
+        this.episodios = episodios;
+    }
+
+    public int getEpisodios() {
+        return this.episodios;
+    }
+
+    public String getGenero() {
+        return this.genero;
+    }
+
+}
---
+++ dev-dojo/maratona-java/src/main/java/javacore/Dconstrutores/dominio/test/AnimeTest01.java
@@ -0,0 +1,9 @@
+package javacore.Dconstrutores.dominio.test;
+import javacore.Dconstrutores.dominio.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Senku","TV",12,"Ação");
+        anime.imprime();
+     }
+}
