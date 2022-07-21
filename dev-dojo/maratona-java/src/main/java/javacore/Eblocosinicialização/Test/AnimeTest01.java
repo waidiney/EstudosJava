f715ffb07a5ed37a49c286900badb5ccab80b5dc
Waidiney <dineyjr@hotmail.com>
2022-07-21 09:43:57 -0300

    Blocos de Inicialização

--- dev-dojo/maratona-java/src/main/java/javacore/Eblocosinicialização/Test/AnimeTest01.java
+++ dev-dojo/maratona-java/src/main/java/javacore/Eblocosinicialização/Test/AnimeTest01.java
@@ -4,7 +4,11 @@ import javacore.Eblocosinicialização.dominio.Anime;
 
 public class AnimeTest01 {
     public static void main(String[] args) {
-        Anime anime = new Anime();
+        Anime anime = new Anime("Love Hina");
         System.out.println(anime.getEpisodios());
+
+        for (int episodio : anime.getEpisodios()) {
+            System.out.print(episodio + " ");
+        }
     }
 }
--- dev-dojo/maratona-java/src/main/java/javacore/Eblocosinicialização/dominio/Anime.java
+++ dev-dojo/maratona-java/src/main/java/javacore/Eblocosinicialização/dominio/Anime.java
@@ -3,15 +3,23 @@ package javacore.Eblocosinicialização.dominio;
 public class Anime {
     private String nome;
     private int[] episodios;
+    // 1 - Alocado espaço na memoria pro objeto
+    // 2 - Cada atributo de classe é inicializao com valores default ou o que for passada
+    // 3 - Bloco de inicialização é executado
+    // 4 - Construtor é executado
     {
         System.out.println("Dentro do bloco de inicialização");
-    }
-
-    public Anime() {
         episodios = new int[100];
         for (int i = 0; i < episodios.length; i++) {
             episodios[i] = i + 1;
         }
+    }
+    public Anime(String nome){
+        this.nome = nome;
+    }
+
+    public Anime() {
+
         for (int episodio: this.episodios) {
             System.out.print(episodio + " ");
         }
