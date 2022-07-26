fedfc97771a11d70f507fda3eaabf2303969ff0a
Waidiney <dineyjr@hotmail.com>
2022-07-26 14:05:21 -0300

    Associação bidirecional

--- dev-dojo/maratona-java/src/main/java/javacore/Gassociacao/dominio/Time.java
+++ dev-dojo/maratona-java/src/main/java/javacore/Gassociacao/dominio/Time.java
@@ -2,9 +2,24 @@ package javacore.Gassociacao.dominio;
 
 public class Time {
     private String nome;
+    private Jogador[] jogadores;
+
 
     public Time(String nome) {
         this.nome = nome;
+    }
+    public Time(String nome, Jogador[] jogadores) {
+        this.nome = nome;
+        this.jogadores = jogadores;
+    }
+
+    public void imprime(){
+        System.out.println(this.nome);
+        if(jogadores == null) return;
+        for (Jogador jogador : jogadores) {
+            System.out.println(jogador.getNome());
+        }
+
     }
 
     public void setNome(String nome) {
@@ -13,5 +28,14 @@ public class Time {
 
     public String getNome() {
         return nome;
+    }
+
+
+    public Jogador[] getJogadores() {
+        return jogadores;
+    }
+
+    public void setJogadores(Jogador[] jogadores) {
+        this.jogadores = jogadores;
     }
 }
---
+++ dev-dojo/maratona-java/src/main/java/javacore/Gassociacao/test/JogadorTest03.java
@@ -0,0 +1,20 @@
+package javacore.Gassociacao.test;
+
+import javacore.Gassociacao.dominio.Jogador;
+import javacore.Gassociacao.dominio.Time;
+
+public class JogadorTest03 {
+    public static void main(String[] args) {
+        Jogador jogador = new Jogador("Cafu");
+        Jogador jogador2 = new Jogador("Pelé");
+        Time time = new Time("Brasil");
+        Jogador[] jogadores = {jogador, jogador2};
+        jogador.setTime(time);
+        jogador2.setTime(time);
+        time.setJogadores(jogadores);
+        System.out.println("---Jogador----");
+        jogador.imprime();
+        System.out.println("---Time---");
+        time.imprime();
+    }
+}
