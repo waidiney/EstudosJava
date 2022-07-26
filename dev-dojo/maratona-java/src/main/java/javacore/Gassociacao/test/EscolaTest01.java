1047383e274a84d56f40b98d57b2122da219e2a0
Waidiney <dineyjr@hotmail.com>
2022-07-26 10:35:08 -0300

    Associação unidirecional muitos para um

---
+++ dev-dojo/maratona-java/src/main/java/javacore/Gassociacao/test/EscolaTest01.java
@@ -0,0 +1,14 @@
+package javacore.Gassociacao.test;
+
+import javacore.Gassociacao.dominio.Escola;
+import javacore.Gassociacao.dominio.Professor;
+
+public class EscolaTest01 {
+    public static void main(String[] args) {
+        Professor professor1 = new Professor("Gustavo Guanabara");
+        Professor professor2 = new Professor("Willian Suane");
+        Professor[] professores = {professor1, professor2};
+        Escola escola = new Escola("Curso em video" , professores);
+        escola.imprime();
+    }
+}
