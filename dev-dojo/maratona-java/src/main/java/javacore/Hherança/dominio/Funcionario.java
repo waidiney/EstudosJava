5c87f165d1156cde3c2edae4434a1f02a843cde8
Waidiney <dineyjr@hotmail.com>
2022-08-03 09:50:56 -0300

    Herança 1.1

--- dev-dojo/maratona-java/src/main/java/javacore/Hherança/dominio/Funcionario.java
+++ dev-dojo/maratona-java/src/main/java/javacore/Hherança/dominio/Funcionario.java
@@ -3,6 +3,11 @@ package javacore.Hherança.dominio;
 public class Funcionario extends Pessoa {
     private double salario;
 
+    public void imprime(){
+        super.imprime();
+        System.out.println(this.salario);
+    }
+
     public double getSalario() {
         return salario;
     }
