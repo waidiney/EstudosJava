53dc87d2640ce2f18482ab838483a33f780ae7ec
Waidiney <dineyjr@hotmail.com>
2022-08-05 16:46:25 -0300

    Sobrescrita do método toString

---
+++ dev-dojo/maratona-java/src/main/java/javacore/Isobrescrita/test/AnimeTest01.java
@@ -0,0 +1,10 @@
+package javacore.Isobrescrita.test;
+
+import javacore.Isobrescrita.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Dr. Stone");
+        System.out.println(anime);
+    }
+}
