58096db0118023d257670369a0c41da5f97d01dc
Waidiney <dineyjr@hotmail.com>
2022-08-08 10:47:00 -0300

    Modificador final  Tipo referência

--- dev-dojo/maratona-java/src/main/java/javacore/Jmodificadorfinal/dominio/Carro.java
+++ dev-dojo/maratona-java/src/main/java/javacore/Jmodificadorfinal/dominio/Carro.java
@@ -3,12 +3,14 @@ package javacore.Jmodificadorfinal.dominio;
 public class Carro {
     private String nome;
     public static final double VELOCIDADE_LIMITE = 250;
-// Toda constante tem que ser escrita em capslock e sepera as palavras com _ e geralmente vem acompanhado de um modificador statico, esta constante por sua vez e usado quando nao querremos alterar o valor da variavel    
-        public String getNome() {
-                return nome;
-        }
+    public final Comprador COMPRADOR = new Comprador();
 
-        public void setNome(String nome) {
-                this.nome = nome;
-        }
+    // Toda constante tem que ser escrita em capslock e sepera as palavras com _ e geralmente vem acompanhado de um modificador statico, esta constante por sua vez e usado quando nao querremos alterar o valor da variavel
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
--- dev-dojo/maratona-java/src/main/java/javacore/Jmodificadorfinal/test/CarroTest01.java
+++ dev-dojo/maratona-java/src/main/java/javacore/Jmodificadorfinal/test/CarroTest01.java
@@ -6,5 +6,8 @@ public class CarroTest01 {
     public static void main(String[] args) {
         Carro carro = new Carro();
         System.out.println(Carro.VELOCIDADE_LIMITE);
+        carro.COMPRADOR.setNome("Jaspion");
+        System.out.println(carro.COMPRADOR);
+
     }
 }
