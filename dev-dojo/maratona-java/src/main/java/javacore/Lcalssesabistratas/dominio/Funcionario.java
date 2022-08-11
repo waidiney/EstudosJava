d5bde73285ce6dc5332a545669ccefe5325f874d
Waidiney <dineyjr@hotmail.com>
2022-08-11 09:17:48 -0300

    Classes abstratas  Métodos abstratos regras

--- dev-dojo/maratona-java/src/main/java/javacore/Lcalssesabistratas/dominio/Funcionario.java
+++ dev-dojo/maratona-java/src/main/java/javacore/Lcalssesabistratas/dominio/Funcionario.java
@@ -1,6 +1,6 @@
 package javacore.Lcalssesabistratas.dominio;
 // Metodos abstratos nao tem corpo, ao usar metodos abstratos voce e obrigado a colocar a implementacao nas subclasses.
-public abstract class Funcionario {
+public abstract class Funcionario extends Pessoa {
     protected String nome;
     protected double salario;
 
@@ -9,6 +9,12 @@ public abstract class Funcionario {
         this.salario = salario;
         calculaBonus();
     }
+
+    @Override
+    public void imprime() {
+        System.out.println("Imprimindo...");
+    }
+
     @Override
     public String toString() {
         return "Funcionario{" +
