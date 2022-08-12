58ee3ed4466880945542b5836ddc38a493ab6baa
Waidiney <dineyjr@hotmail.com>
2022-08-12 16:44:31 -0300

    Polimorfismo Parâmetros polimórficos
    diminui a quantidade de codigos deixando o mais limpo, diminuindo a quantidade de metodos

--- dev-dojo/maratona-java/src/main/java/javacore/Npolimorfismo/Test/ProdutoTest01.java
+++ dev-dojo/maratona-java/src/main/java/javacore/Npolimorfismo/Test/ProdutoTest01.java
@@ -1,6 +1,7 @@
 package javacore.Npolimorfismo.Test;
 
 import javacore.Npolimorfismo.dominio.Computador;
+import javacore.Npolimorfismo.dominio.Televisao;
 import javacore.Npolimorfismo.dominio.Tomate;
 import javacore.Npolimorfismo.servico.CalculadoraImposto;
 
@@ -8,8 +9,13 @@ public class ProdutoTest01 {
     public static void main(String[] args) {
         Computador computador = new Computador("Ibyte Active 7SE", 1400);
         Tomate tomate = new Tomate("Tomate Cereja", 5);
-        CalculadoraImposto.calcularImpostoComputador(computador);
+        Televisao tv = new Televisao("Samsung 50\" ", 5000);
+
+
+        CalculadoraImposto.calcularImposto(computador);
         System.out.println("---------------------------------------------");
-        CalculadoraImposto.calcularImpostoTomate(tomate);
+        CalculadoraImposto.calcularImposto(tomate);
+        System.out.println("---------------------------------------------");
+        CalculadoraImposto.calcularImposto(tv);
     }
 }
--- dev-dojo/maratona-java/src/main/java/javacore/Npolimorfismo/servico/CalculadoraImposto.java
+++ dev-dojo/maratona-java/src/main/java/javacore/Npolimorfismo/servico/CalculadoraImposto.java
@@ -1,22 +1,18 @@
 package javacore.Npolimorfismo.servico;
 
 import javacore.Npolimorfismo.dominio.Computador;
+import javacore.Npolimorfismo.dominio.Produto;
 import javacore.Npolimorfismo.dominio.Tomate;
 
-// Pacote servico foi criado porque qaundo trabalhamos com arquitetura NVC( ou a regra de negocios) , voce sepera o modelo (dominio) que representa o banco de dados, com o que voce vai vizualizar( o test), neste caso a regra de negocios e fazer um modelo pra calular os impostos.
+// Pacote servico foi criado porque quando trabalhamos com arquitetura NVC( ou a regra de negocios) , voce separa o modelo (dominio) que representa o banco de dados, com o que voce vai vizualizar( o test), neste caso a regra de negocios e fazer um modelo pra calular os impostos.
 public class CalculadoraImposto {
-    public static void calcularImpostoComputador(Computador computador){
-        System.out.println("Relatorio de imposto do computador");
-        double imposto = computador.calcularImposto();
-        System.out.println("Computador "+computador.getNome());
-        System.out.println("Valor "+computador.getValor());
-        System.out.println("Imposto a ser pago "+imposto);
-    }
-    public static void calcularImpostoTomate(Tomate tomate){
-        System.out.println("Relatorio de imposto do computador");
-        double imposto = tomate.calcularImposto();
-        System.out.println("Tomate "+tomate.getNome());
-        System.out.println("Valor "+tomate.getValor());
-        System.out.println("Imposto a ser pago "+imposto);
+
+    public static void calcularImposto(Produto produto) {
+        System.out.println("Relatorio de imposto");
+        produto.calcularImposto();
+        double imposto = produto.calcularImposto();
+        System.out.println("Produto: " + produto.getNome());
+        System.out.println("Preco: " + produto.getValor());
+        System.out.println("Imposto a ser pago: " + imposto);
     }
 }
