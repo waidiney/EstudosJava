3aac8d5371792776a646d1494423613d5467853e
Waidiney <dineyjr@hotmail.com>
2022-08-30 10:39:46 -0300

    Exceções - RuntimeException - ArrayIndexOutOfBounds

--- dev-dojo/maratona-java/src/main/java/javacore/Oexception/runtime/test/RuntimeExceptionTest01.java
+++ dev-dojo/maratona-java/src/main/java/javacore/Oexception/runtime/test/RuntimeExceptionTest01.java
@@ -3,7 +3,7 @@ package javacore.Oexception.runtime.test;
 public class RuntimeExceptionTest01 {
     public static void main(String[] args) {
         //Checked e Unchecked
-        Object object = null;
-        System.out.printf(object.toString());
+        int[] nums = {1,2};
+        System.out.println(nums[2]);
     }
 }
