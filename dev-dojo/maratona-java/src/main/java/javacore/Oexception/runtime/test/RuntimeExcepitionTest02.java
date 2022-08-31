7e0a07300e7e7507bb21a1e80f25eb546be918bf
Waidiney <dineyjr@hotmail.com>
2022-08-31 08:59:05 -0300

    Exceções - Lançando exceção unchecked

--- dev-dojo/maratona-java/src/main/java/javacore/Oexception/runtime/test/RuntimeExcepitionTest02.java
+++ dev-dojo/maratona-java/src/main/java/javacore/Oexception/runtime/test/RuntimeExcepitionTest02.java
@@ -2,17 +2,25 @@ package javacore.Oexception.runtime.test;
 
 public class RuntimeExcepitionTest02 {
     public static void main(String[] args) {
-        System.out.println(divisao(1,0));
+        divisao(1,0);
     }
+
+    /**
+     *
+     * @param a
+     * @param b nao pode ser zero
+     * @return
+     * @throws IllegalArgumentException caso b seja zero
+     */
     private static int divisao(int a, int b){
         if (b == 0){
-            throw new RuntimeException("Argumento ilegal, nao pode ser 0");
+            throw new IllegalArgumentException("Argumento ilegal, nao pode ser 0");
         }
         try{
-            return a/b ;
-        }catch (RuntimeException e){ // foi usado RuntimeException pq ele eh uma super classe de ArithmeticException.
+            return a/b ; 
+        }catch (RuntimeException e){
             e.printStackTrace();
         }
-        return 0;
+        return a/b;
     }
 }
